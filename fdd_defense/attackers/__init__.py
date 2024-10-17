9a3a0fcbb3cd1ea42393de7487afc982ab325c14
Aleksandr <strangequark173@gmail.com>
2024-10-17 11:57:32 +0300

    обновлен init

--- fdd_defense/attackers/__init__.py
+++ fdd_defense/attackers/__init__.py
@@ -5,6 +5,7 @@ from .carlini_wagner import CarliniWagnerAttacker
 from .deep_fool import DeepFoolAttacker
 from .noattack import NoAttacker
 from .distillation import DistillationBlackBoxAttacker
+from .deep_lift import DeepLIFTAttack
 
 __all__ = [
     'FGSMAttacker',
