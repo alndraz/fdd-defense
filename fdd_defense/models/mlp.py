65228d9635b4665220481c7dbabefcc27a89fe9b
Aleksandr <strangequark173@gmail.com>
2024-10-17 14:30:23 +0300

    update mlp

--- fdd_defense/models/mlp.py
+++ fdd_defense/models/mlp.py
@@ -1,31 +1,35 @@
 from torch import nn
 from fdd_defense.models.base import BaseTorchModel
 
+
 class MLP(BaseTorchModel):
     def __init__(
-            self, 
-            window_size: int, 
-            step_size: int, 
+            self,
+            window_size: int,
+            step_size: int,
             batch_size=128,
             lr=0.001,
             num_epochs=10,
             is_test=False,
             device='cpu',
             hidden_dim=624,
-        ):
+    ):
         super().__init__(
             window_size, step_size, batch_size, lr, num_epochs, is_test, device,
         )
         self.hidden_dim = hidden_dim
 
-    def fit(self, dataset):
-        super().fit(dataset)
-        num_sensors = self.dataset.df.shape[1]
-        num_states = len(set(self.dataset.label))
+        # Инициализация архитектуры модели в конструкторе
+        num_sensors = 52
+        num_states = 21
+
         self.model = nn.Sequential(
             nn.Flatten(),
             nn.Linear(num_sensors * self.window_size, self.hidden_dim),
             nn.ReLU(),
             nn.Linear(self.hidden_dim, num_states),
         )
+
+    def fit(self, dataset):
+        super().fit(dataset)
         super()._train_nn()
