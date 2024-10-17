316d99ba862aa5ce6b5c442c3cfe1142fcaaf42e
Aleksandr <strangequark173@gmail.com>
2024-10-17 17:17:46 +0300

    update

--- fdd_defense/attackers/deep_lift.py
+++ fdd_defense/attackers/deep_lift.py
@@ -1,10 +1,11 @@
 from fdd_defense.attackers.base import BaseAttacker
 import numpy as np
 
+
 class DeepLIFTAttack(BaseAttacker):
-    def __init__(self, model, eps, selected_indices):
+    def __init__(self, model, eps, selected_indices, attributions_time, interpretation_threshold):
         """
-        Инициализация атакера, добавляющего шум к выбранным признакам на всех временных шагах.
+        Инициализация атаки на основе интерпретации DeepLIFT.
 
         Parameters:
         ----------
@@ -14,13 +15,19 @@ class DeepLIFTAttack(BaseAttacker):
             Максимальное смещение (шум), которое можно добавить к данным.
         selected_indices: list
             Список индексов признаков, которые будут атакованы.
+        attributions_time: dict
+            Словарь с важностями признаков на каждом временном шаге для выбранных признаков.
+        interpretation_threshold: float
+            Порог для интерпретации важности. Признаки с важностью выше этого порога будут атакованы.
         """
         super().__init__(model, eps)
         self.selected_indices = selected_indices
+        self.attributions_time = attributions_time
+        self.interpretation_threshold = interpretation_threshold
 
     def attack(self, ts, label):
         """
-        Реализация атаки с добавлением шума выбранным признакам на всех временных шагах.
+        Реализация атаки с добавлением шума на основе интерпретации важности признаков.
 
         Parameters:
         ----------
@@ -39,11 +46,13 @@ class DeepLIFTAttack(BaseAttacker):
 
         # Проходим по каждому временному шагу
         for t in range(perturbed_ts.shape[1]):  # Итерируем по временным шагам
-            # Для каждого выбранного признака добавляем шум на всех временных шагах
+            # Для каждого признака из выбранных индексов проверяем важность на этом шаге
             for index in self.selected_indices:
-                # Генерируем шум для атаки
-                noise = self.eps * np.random.choice([1, -1], size=ts.shape[0])
-                # Добавляем шум к текущему значению признака на данном временном шаге
-                perturbed_ts[:, t, index] += noise
+                # Проверяем, превышает ли важность порог для данного признака на текущем временном шаге
+                if abs(self.attributions_time[index][t]) > self.interpretation_threshold:
+                    # Генерируем шум для атаки
+                    noise = self.eps * np.random.choice([1, -1], size=ts.shape)
+                    # Добавляем шум к текущему значению признака на данном временном шаге
+                    perturbed_ts[:, t, index] += noise
 
         return perturbed_ts
