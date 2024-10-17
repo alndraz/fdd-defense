545b7d2f519704d82bd796dd6df03e48c9d300ce
Aleksandr <strangequark173@gmail.com>
2024-10-17 14:50:34 +0300

    update mlp

--- fdd_defense/models/mlp.py
+++ fdd_defense/models/mlp.py
@@ -1,11 +1,10 @@
 from torch import nn
 from fdd_defense.models.base import BaseTorchModel
 
-
 class MLP(BaseTorchModel):
     def __init__(
             self,
-            window_size: int,   # Параметр window_size
+            window_size: int,
             step_size: int,
             batch_size=128,
             lr=0.001,
@@ -13,26 +12,20 @@ class MLP(BaseTorchModel):
             is_test=False,
             device='cpu',
             hidden_dim=624,
-    ):
+        ):
         super().__init__(
             window_size, step_size, batch_size, lr, num_epochs, is_test, device,
         )
         self.hidden_dim = hidden_dim
-        self.device = device
-        self.window_size = window_size  # Не забывайте сохранять этот параметр
-
-        # Инициализация архитектуры модели в конструкторе
-        num_sensors = 52  # Количество сенсоров (можно изменить на основе данных)
-        num_states = 21  # Количество классов (также может зависеть от задачи)
-
-        # Определение модели
-        self.model = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(num_sensors * self.window_size, self.hidden_dim),  # Использование window_size
-            nn.ReLU(),
-            nn.Linear(self.hidden_dim, num_states),
-        ).to(self.device)  # Перенос модели на устройство (GPU или CPU)
 
     def fit(self, dataset):
         super().fit(dataset)
+        num_sensors = self.dataset.df.shape[1]
+        num_states = len(set(self.dataset.label))
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(num_sensors * self.window_size, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, num_states),
+        )
         super()._train_nn()
